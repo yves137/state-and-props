3833e8cafd1f98db3de6665317b220666ff08367
yves137 <oneg2981@gmail.com>
2023-10-03 16:37:39 +0200

    update project

--- src/CustomButtons.js
+++
@@ -1,8 +0,0 @@
-export function CustomButtons({handleClick}){
-   return <button
-        onClick={() => handleClick()}
-        className="border border-black bg-slate-300 py-2 px-5 hover:bg-slate-200 rounded-md"
-      >
-        Click Me
-      </button>
-}
